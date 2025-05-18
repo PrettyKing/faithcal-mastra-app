931677b1c78cc4b5784e1258650e2de22ac490c8
chalee <695469701@qq.com>
2025-05-18 18:33:36 +0800

    feat: update config

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,6 +2,7 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { LibSQLStore } from '@mastra/libsql';
+import { CloudflareDeployer } from '@mastra/deployer-cloudflare';
 
 import { weatherAgent } from './agents/weather';
 import { codeReviewAgent } from './agents/code-review';
@@ -15,5 +16,13 @@ export const mastra = new Mastra({
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
+  }),
+  deployer: new CloudflareDeployer({
+    scope: process.env.CLOUDFLARE_ACCOUNT_ID || '',
+    projectName: process.env.CLOUDFLARE_PROJECT_NAME,
+    auth: {
+      apiToken: process.env.CLOUDFLARE_API_TOKEN || '',
+      apiEmail: process.env.CLOUDFLARE_EMAIL,
+    },
   }),
 });
